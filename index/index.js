e0dfd29ecc8d5b5adeb164cc2990f96ad0284fed
tsinaaa <tsinukaldeboch5434@gmail.com>
2025-02-02 12:59:40 +0300

    included echofy services and more

---
+++ index/index.js
@@ -0,0 +1,8 @@
+let serviceBtn = document.getElementById("service-btn")
+serviceBtn.addEventListener("click", function() {
+    alert("button clicked")
+})
+
+function display() {
+    alert("button clicked")
+}
